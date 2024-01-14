16c6280dae6d863d7c2f50bc795fdf149fa4fae6
alex sanchez <alexjsanchez1020@gmail.com>
2024-01-14 03:19:35 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
 import logo from "./alex_crop.jpeg";
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import "../src/App.css";
 
-var i = 0;
-
-function move(stats) {
-	if (i == 0) {
-		i = 1;
-		var myAir = document.getElementById("myAir");
-		var width = 1;
-		var id = setInterval(frame, 10);
-		function frame() {
-			if (width >= stats.air) {
-				clearInterval(id);
-				i = 0;
-			} else {
-				width++;
-				myAir.style.width = width + "%";
-			}
-		}
-	}
-}
-
+const data = [
+	{ name: "Air", score: "60%" },
+	{ name: "Hangtime", score: "40%" },
+	{ name: "Ollie", score: "60%" },
+	{ name: "Speed", score: "80%" },
+	{ name: "Spin", score: "40%" },
+	{ name: "Landing", score: "60%" },
+	{ name: "Switch", score: "20%" },
+	{ name: "Rail Balance", score: "50%" },
+	{ name: "Lip Balance", score: "80%" },
+	{ name: "Manuals", score: "40%" },
+];
 function App() {
-	const [stats, setStats] = useState({ air: 50 });
+	const [stats, setStats] = useState([
+		{ name: "Air", score: "0%" },
+		{ name: "Hangtime", score: "0%" },
+		{ name: "Ollie", score: "0%" },
+		{ name: "Speed", score: "0%" },
+		{ name: "Spin", score: "0%" },
+		{ name: "Landing", score: "0%" },
+		{ name: "Switch", score: "0%" },
+		{ name: "Rail Balance", score: "0%" },
+		{ name: "Lip Balance", score: "0%" },
+		{ name: "Manuals", score: "0%" },
+	]);
+	useEffect(() => {
+		setStats(data);
+	}, []);
+
 	return (
-		<div className="App">
+		<div className="App flex flex-col items-center">
 			<h1>skater stat</h1>
-			<div className="flex justify-center items-center">
-				<img alt="alex walking" src={logo} />
-				<div id="myProgress">
-					<div id="myBox" className="myAir"></div>
-					<div id="myBox" className="myHangtime"></div>
+			<div className="flex items-center">
+				<div>
+					<img alt="alex walking" src={logo} />
+				</div>
+				<div className="w-full">
+					{stats.map((stat) => {
+						return (
+							<div>
+								<p>{stat.name}</p>
+								<div className="loader-container">
+									<div
+										className="loader-bar flex justify-end"
+										style={{
+											width: stat.score,
+										}}
+									></div>
+								</div>
+							</div>
+						);
+					})}
 				</div>
 			</div>
-
-			<button onClick={() => move(stats)}>Click Me</button>
+			<div className="flex justify-center">
+				<iframe
+					width="460px"
+					src="https://www.youtube.com/embed/cmhP2i1aK_k?si=dIvzdlq0dbWED7Jy"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
+			</div>
 		</div>
 	);
 }
