e73ca3e8b43958a02b00c97f4566d0949e2b22b6
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2024-02-14 04:13:29 -0500

    stable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import express
 const express = require("express");
 const cors = require("cors");
-
+const { ObjectId } = require("mongodb");
 // connect to data base
 const { connectToDb, getDb } = require("./src/db");
 //instances of express app
@@ -21,26 +21,53 @@ server.use(cors());
 let db;
 
 connectToDb((err) => {
-	if (!err) {
-		server.listen(5000, () => {
-			console.log("port is listening on 5000");
-		});
-		db = getDb();
-	}
+  if (!err) {
+    server.listen(3000, () => {
+      console.log("port is listening on 3000");
+    });
+    db = getDb();
+  }
 });
 
 server.get("/books", (req, res) => {
-	let books = [];
-	db.collection("books")
-		.find()
-		.sort({ tracks: 1 })
-		.forEach((book) => {
-			books.push(book);
-		})
-		.then(() => {
-			res.status(200).json(books);
-		})
-		.catch((err) => {
-			res.status(500).json({ error: "could not detch the documents" });
-		});
+  let books = [];
+  db.collection("books")
+    .find()
+    .sort({ tracks: 1 })
+    .forEach((book) => {
+      books.push(book);
+    })
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "could not detch the documents" });
+    });
+});
+
+server.get("/books", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collections("books")
+      .findOne({ _id: req.params.id })
+      .then((doc) => {
+        res.status(200).json(res);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "cant fetch" });
+      });
+  } else {
+    res.status(400).json({ error: "not a valid id" });
+  }
+});
+
+server.post("/books", (req, res) => {
+  const books = req.body;
+  db.collection("books")
+    .insertOne(books)
+    .then((res) => {
+      res.status(201).json({ message: "successful insert" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "could not create a new document" });
+    });
 });
